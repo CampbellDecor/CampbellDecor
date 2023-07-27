841a961dbd8fb495216544d18c2ebcc68e881ebe
YPPINTHU <pinthushan71998@gmail.com>
2023-07-27 23:37:19 +0530

    Dark mode Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:campbelldecor/screens/homescreen.dart';
-import 'package:campbelldecor/screens/ltest1.dart';
 import 'package:campbelldecor/screens/notifications/notification_setup.dart';
 import 'package:campbelldecor/screens/notifications/notificationscreen.dart';
-import 'package:campbelldecor/screens/test.dart';
 import 'package:campbelldecor/screens/theme/theme_colors.dart';
 import 'package:campbelldecor/screens/theme/theme_manager.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 
 final navigatorKey = GlobalKey<NavigatorState>();
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -20,23 +18,37 @@ void main() async {
   runApp(const MyApp());
 }
 
-ThemeManager _themeMode = ThemeManager();
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeClass.lightTheme,
-      darkTheme: ThemeClass.darkTheme,
-      themeMode: _themeMode.themeMode,
-      home: const HomeScreen(),
-      navigatorKey: navigatorKey,
-      routes: {
-        NotificationScreen.route: (context) => const NotificationScreen(),
-      },
-    );
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => ThemeManager()),
+        ],
+        child: Builder(
+          builder: (BuildContext context) {
+            final themeManager = Provider.of<ThemeManager>(context);
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Flutter Demo',
+              theme: ThemeClass.lightTheme,
+              darkTheme: ThemeClass.darkTheme,
+              themeMode: themeManager.themeMode,
+              home: HomeScreen(),
+              navigatorKey: navigatorKey,
+              routes: {
+                NotificationScreen.route: (context) =>
+                    const NotificationScreen(),
+              },
+            );
+          },
+        ));
   }
 }
